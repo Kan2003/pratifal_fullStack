8274eae3c5f49f93e7bf42993d00c3f17a7ee4f4
Kanha Vishwakarma <122142009+Kan2003@users.noreply.github.com>
2024-10-22 10:27:41 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy : {
-      '/api' : 'https://pratifal-backend.vercel.app/'
+    proxy: {
+      '/api': {
+        target: 'https://pratifal-backend.vercel.app',
+        changeOrigin: true,
+        secure: true, // If your backend uses HTTPS
+        rewrite: (path) => path.replace(/^\/api/, '') // Adjust this if needed
+      }
     }
   },
   plugins: [react()],
