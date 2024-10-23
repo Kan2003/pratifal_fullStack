cb5618475ee1d162206ef04eea65eb9a9b875623
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-10-23 13:38:04 +0530

    error ui fixed

--- Frontend/src/components/littleComponents/Error.jsx
+++ Frontend/src/components/littleComponents/Error.jsx
@@ -3,9 +3,9 @@ import cross from "../../assets//cross-mark-svgrepo-com.svg";
 
 const Error = ({error}) => {
   return (
-    <div className=" absolute transition-all duration-300 ease-in top-5 left-1/2 z-[1000] flex items-center gap-2 w-fit -translate-x-1/2 rounded-lg bg-[#dad8d891] py-2 px-6">
-    <img className="w-[20px] h-[20px]" src={cross} alt="" />
-    <p className="text-black text-center">{error}</p>
+    <div className=" absolute transition-all duration-300 ease-in top-5 left-1/2 z-[1000] flex items-center gap-2 w-fit -translate-x-1/2 rounded-lg bg-[#dad8d8c5] py-2 px-6">
+    <img className="w-[20px] xs:w-[15px] xs:h-[15px] sm:w-[15px] sm:h-[15px] h-[20px]" src={cross} alt="" />
+    <p className="text-black xs:text-[12px] sm:text-[15px] text-center">{error}</p>
   </div>
   )
 }
